ff9f1a71852e09b1a5448f8d776dce1ddf4dcdf2
namonroyr <namonroyr@unal.edu.co>
2022-01-23 18:15:01 -0500

    EOL file correction

--- ODBC_driver.sh
+++ ODBC_driver.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 sudo su
 curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
-
 #Download appropriate package for the OS version
 #Choose only ONE of the following, corresponding to your OS version
-
 #Debian 9
 curl https://packages.microsoft.com/config/debian/9/prod.list > /etc/apt/sources.list.d/mssql-release.list
-
 exit
 sudo apt-get update
 sudo ACCEPT_EULA=Y apt-get install -y msodbcsql17
