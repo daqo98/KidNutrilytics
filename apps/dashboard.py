0455bd527c090e973f7c51c567b8237504cef4d8
namonroyr <namonroyr@unal.edu.co>
2022-01-22 23:34:40 -0500

    SQL Queries to import data on dashboard and Malnutrition tab

--- apps/dashboard.py
+++ apps/dashboard.py
@@ -14,17 +14,123 @@ import requests
 import math
 import ssl
 import json
+import pyodbc
+
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
 with urlopen('https://raw.githubusercontent.com/namonroyr/colombia_mapa/master/co_2018_MGN_DPTO_POLITICO.geojson') as response:
     colombia = json.load(response)
 
-mapa = requests.get("https://mapsmicroservice-zbca65qbuq-nn.a.run.app/api/v1/maps")
-dpts_count = pd.DataFrame.from_dict(mapa.json())
+"""
+-------------------------------------------SQL Connection------------------------------------
+"""
+server = 'ds4a-server2.database.windows.net'
+database = 'ds4a'
+username = 'namonroyr'
+password = 'ds4a123*'
+driver= '{ODBC Driver 17 for SQL Server}'
+
+conn = pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
+
+def execute_azure_query(query, with_cursor=False):
+    cursor = conn.cursor()
+    try:
+        cursor.execute(query)
+        res = cursor.fetchall()
+        if with_cursor:
+            return (res, cursor)
+        else:
+            return res
+    finally:
+        pass
+        cursor.close()
 
 
-fig_years_dist = plot_by_year.ploting_distribution()
+def pandas_df_from_azure_query(query):
+    df = pd.read_sql(query,conn)
+    return df
+
+#Se filtran beneficiarios cuyo target de relapse para los próximos 6 meses sea 1
+df_target1_relapse = pandas_df_from_azure_query(
+"""
+SELECT *
+FROM base_target_2018
+WHERE Marca_Target_Reincidencia_F6M == 1
+"""
+)
+
+#Se filtran beneficiarios cuyo target de desnutrición para los próximos 6 meses sea 1
+df_target1_mal = pandas_df_from_azure_query(
+"""
+SELECT *
+FROM base_target_2018
+WHERE Marca_Target_EntroEnDesnutricion_F6M == 1
+"""
+)
+
+#Se agrupan los target de relapse por dpto para obtener el count
+dpts_count_target1_relapse = pandas_df_from_azure_query(
+"""
+SELECT cod_dpto, nom_dpto, COUNT(0) AS Count_Dpto_Relapse
+FROM df_target1_relapse
+GROUP BY 1,2
+ORDER BY 1,2
+"""
+)
+
+#Se agrupan los target de malnutrition por dpto para obtener el count
+dpts_count_target1_mal = pandas_df_from_azure_query(
+"""
+SELECT cod_dpto, nom_dpto, COUNT(0) AS Count_Dpto_Malnutrition
+FROM df_target1_mal
+GROUP BY 1,2
+ORDER BY 1,2
+"""
+)
+
+#Se agrupan el count total de registros suministrados por dpto
+dpts_count_total = pandas_df_from_azure_query(
+"""
+SELECT cod_dpto, nom_dpto, COUNT(0) AS Count_Dpto_Total
+FROM base_target_2018
+GROUP BY 1,2
+ORDER BY 1,2
+"""
+)
+
+#Se unen los 3 dfs creados anteriormente
+df_complete = pandas_df_from_azure_query(
+"""
+SELECT * FROM dpts_count_target1_relapse UNION ALL
+SELECT * FROM dpts_count_target1_mal UNION ALL
+SELECT * FROM dpts_count_total
+"""
+)
+
+#Se crea una columna con el ratio entre count relapse y total por dept
+dpts_count = pandas_df_from_azure_query(
+"""
+SELECT CAST(cod_dpto AS INT), nom_dpto, Count_Dpto_Total, Count_Dpto_Malnutrition, Count_Dpto_Relapse,
+  Count_Dpto_Relapse/Count_Dpto_Total AS count_ratio_relapse,
+  Count_Dpto_Malnutrition/Count_Dpto_Total AS count_ratio_mal
+FROM df_complete
+"""
+)
+
+
+#mapa = requests.get("https://mapsmicroservice-zbca65qbuq-nn.a.run.app/api/v1/maps")
+#dpts_count = pd.DataFrame.from_dict(mapa.json())
+
+####PLot by year
+base_pivot = pandas_df_from_azure_query(
+"""
+SELECT *
+FROM tomas_pivot
+"""
+)
+
+fig_years_dist = plot_by_year.ploting_distribution(base_pivot)
 
 years = dpts_count['Año'].unique()
 slider_items = {int(math.floor(years[i])):str(math.floor(years[i])) for i in range(len(years))}
@@ -39,12 +145,12 @@ controlslider = html.Div([
         allowCross=False,
         marks=slider_items,
         value=2017,
-    )  
+    )
 ])
 """
 
 card_map = dbc.Card(
-    dbc.CardBody([    
+    dbc.CardBody([
         html.H4("Malnutrition Relapse % by Department"),
         html.Hr(id="hr_1"),
         # html.Div(control_dropdown),
@@ -53,12 +159,12 @@ card_map = dbc.Card(
                 id='colombia_plot',
                 figure={}
             )
-        )     
+        )
     ])
 ,color="primary", outline=True)
 
 card_map2 = dbc.Card(
-    dbc.CardBody([    
+    dbc.CardBody([
         html.H4("Malnutrition % by Department"),
         html.Hr(id="hr_1"),
         html.Div(
@@ -66,7 +172,7 @@ card_map2 = dbc.Card(
                 id='colombia_plot_2',
                 figure={}
             )
-        )     
+        )
     ])
 ,color="primary", outline=True)
 
--- utils/plot_by_year.py
+++ utils/plot_by_year.py
@@ -4,7 +4,22 @@ from itertools import cycle
 import requests
 
 
-def ploting_distribution():
+def nombres(x):
+    if x == 'Desnutricion Aguda Moderada' or x == 'Desnutricion aguda severa':
+        x = 'Desnutricion'
+    else:
+        x
+    return x
+
+def ploting_distribution(base_pivot):
+    base_pivot['EstadoPesoTalla_New'] = base_pivot['EstadoPesoTalla'].apply(nombres)
+    base_pivot['FechaValoracionNutricional'] = pd.to_datetime(base_pivot['FechaValoracionNutricional'])
+    base_pivot['FechaValoracionNutricional_MesAño'] = base_pivot['FechaValoracionNutricional'].dt.to_period('M')
+    frecuencias_mes = base_pivot[['FechaValoracionNutricional_MesAño','EstadoPesoTalla_New']].groupby(['FechaValoracionNutricional_MesAño','EstadoPesoTalla_New']).size()
+    porcentajes_mes = frecuencias_mes.groupby(level=0).apply(lambda x : 100 * x / float(x.sum()))
+    porcentajes_mes = porcentajes_mes.reset_index()
+    porcentajes_mes.columns = ['FechaValoracionNutricional_MesAño', 'EstadoPesoTalla_New', 'Porcentajes']
+    """
     ry = requests.get('https://microservice1-zbca65qbuq-nn.a.run.app/api/v1/y')
     y = ry.json()
     rx = requests.get('https://microservice1-zbca65qbuq-nn.a.run.app/api/v1/x')
@@ -15,9 +30,19 @@ def ploting_distribution():
     y2 = y[12:24]
     y3 = y[24:]
     fig = px.line(porcentajes_mes, x=x, y=[y1,y2,y3], color_discrete_sequence=px.colors.sequential.Aggrnyl_r)
-    names = cycle(['Malnutrition 2017', 'Malnutrition 2018', 'Malnutrition 2019']) 
+    names = cycle(['Malnutrition 2017', 'Malnutrition 2018', 'Malnutrition 2019'])
     fig.for_each_trace(lambda x : x.update(name = next(names)))
     fig.update_layout(xaxis_title="Month", yaxis_title="Percentage")
     fig.update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0)', 'paper_bgcolor': 'rgba(0, 0, 0, 0)'})
     fig.update_layout(legend=dict(x=0.1,y=1))
+    """
+    x = porcentajes_mes['FechaValoracionNutricional_MesAño'].unique().strftime('%Y-%m')
+    y1 = porcentajes_mes[porcentajes_mes['EstadoPesoTalla_New'] == 'Desnutricion']['Porcentajes']
+    fig = px.line(porcentajes_mes, x=x, y=y1, color_discrete_sequence=px.colors.sequential.Aggrnyl_r)
+    names = cycle(['Malnutrition 2017', 'Malnutrition 2018', 'Malnutrition 2019'])
+    #fig.for_each_trace(lambda x : x.update(name = next(names)))
+    fig.update_layout(xaxis_title="Month", yaxis_title="Percentage")
+    fig.update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0)', 'paper_bgcolor': 'rgba(0, 0, 0, 0)'})
+    fig.update_layout(legend=dict(x=0.1,y=1))
+    fig.show()
     return fig
